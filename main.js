2ab2d212024dc4b6ab17dd87a30459f1c497657d
aziz <erazizeee@gmail.com>
2022-09-14 12:51:06 +0530

    Date-in-js

---
+++ main.js
@@ -0,0 +1,40 @@
+
+// const date = new Date();
+// document.getElementById("da").innerHTML = date.toISOString();
+// console.log(date);
+
+
+
+// Date()
+// document.getElementById('da').innerHTML = value1;
+// document.getElementById('da2').innerHTML = value2;
+// document.getElementById('da3').innerHTML = value2;
+
+
+// value1.innerText = date.getDate()
+// value2.innerText = now.getMonth()
+// value3.innerText = now.getFullYear()
+// const date = Date().toString();
+
+const now = new Date();
+
+console.log(now.getDate());
+console.log(now.getMonth());
+console.log(now.getFullYear());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+
+// document.getElementById('da').innerHTML.innerHTML = (now.getDate());
+
+
+
+// const [month, date, year] = new Date().
+//     toLocaleDateString("en-IN").split("/");
+// console.log(new Date().toLocaleDateString("en-IN").split("/"));
+// console.log(month, date, year);
+
+// const date = new Date();
+// const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+// const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+
